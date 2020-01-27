a3ba2962ee15a296ed7eecaf0b4e8480f596a979
Emanuel F. Oliveira <emfolg@gmail.com>
2020-01-27 04:06:23 -0300

    WIP Icons + Text

--- js/app/config.js
+++ js/app/config.js
@@ -3,6 +3,7 @@ define(function () {
   return {
     i18n: {
       pt_BR: {
+        medical_imaging_on_the_web: 'Imagens Médicas na Web \uD83D\uDC89',
         who_am_i: 'Quem és tu, mancebo?',
         human_30y_plus: 'Humano que passou dos 30...',
         father_of_a_boy: 'Pai de um garotinho maravilhoso!',
@@ -10,8 +11,9 @@ define(function () {
         works_with_web_for_10y_plus: 'Trabalha com Web há mais de 10 anos...',
         works_with_medica_imaging_for_3y_plus: '... e com imagens médicas há mais de 3 anos!',
         motivation: 'Motivação',
-        draw_community_attention_to_medical_imaging: 'Trazer atenção da comunidade para a área de imagens médicas',
-        advertise_open_source_medical_imaging_projects: '... e para seus projetos open source!',
+        advertise_open_source_medical_imaging_projects: 'Divulgar projetos open-source da área de imagens médicas',
+        draw_community_attention_to_medical_imaging: 'Despertar interesse da comunidade para a área',
+        make_the_subject_less_obscure: 'Tornar o assunto menos obscuro',
         what_some_think_it_is: 'Imagina-se que seja isso:',
         or_this: '... e isso:',
         what_it_actually_is: 'Na verdade é isso:'
